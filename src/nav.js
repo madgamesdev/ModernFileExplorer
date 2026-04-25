fa8538ed71d7b683bf844da8eb51654d1c11469c
madgamesdev <164058621+madgamesdev@users.noreply.github.com>
2026-04-25 11:37:37 +0300

    ModernFileExplorer 0.0.3 Release!

--- src/nav.js
+++ src/nav.js
@@ -3,23 +3,31 @@ const { addressBar, backButton, forwardButton } = require('./ui')
 const { navigateTo } = require('./dir')
 
 function initNavigation() {
-    backButton.onclick = () => {
-        if (state.historyIndex <= 0) return
-        state.historyIndex--
-        navigateTo(state.history[state.historyIndex], false)
-    }
+    backButton.onclick = () => { goBack() }
 
-    forwardButton.onclick = () => {
-        if (state.historyIndex >= state.history.length - 1) return
-        state.historyIndex++
-        navigateTo(state.history[state.historyIndex], false)
-    }
+    forwardButton.onclick = () => { goForward() }
 
     addressBar.addEventListener('keydown', (e) => {
         if (e.key === "Enter") navigateTo(addressBar.value)
     })
 
     addressBar.addEventListener('focus', () => addressBar.select())
+
+    window.addEventListener('mousedown', (e) => {
+        if (e.button === 3) { goBack() }
+        else if (e.button === 4) { goForward() }
+    })
+}
+
+function goBack() {
+    if (state.historyIndex <= 0) return
+    state.historyIndex--
+    navigateTo(state.history[state.historyIndex], false)
+}
+function goForward() {
+    if (state.historyIndex >= state.history.length - 1) return
+    state.historyIndex++
+    navigateTo(state.history[state.historyIndex], false)
 }
 
 module.exports = { initNavigation }
