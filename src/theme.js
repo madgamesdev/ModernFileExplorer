9ff1e175a93ba08a0a0e35aaf272f43e212e2d98
madgamesdev <164058621+madgamesdev@users.noreply.github.com>
2026-05-01 23:45:18 +0300

    Win 11 Fixes

--- src/theme.js
+++ src/theme.js
@@ -1,7 +1,42 @@
 const { ipcRenderer } = require('electron')
 const { adjustColorBrightness } = require('./utils')
+const os = require('os')
+const { getUI } = require('./ui')
+
+function setWindowBorderUp() {
+    let style = 0
+
+    const release = os.release()
+    const build = parseInt(release.split('.')[2] || '0', 10)
+
+    if (process.platform === 'win32' && build >= 22000) {
+        style = 2 // Windows 11
+    } else if (process.platform === 'win32') {
+        style = 1 // Windows 10
+    } else {
+        style = 0 // not Windows
+    }
+
+    const ui = getUI()
+
+    if (ui.border) {
+        switch (style) {
+            case 1:
+                ui.border.style.borderRadius = '0px'
+                break
+            case 2:
+                ui.border.style.borderRadius = '8px'
+                break
+            default:
+                ui.border.style.display = 'none'
+                break
+        }
+    }
+}
 
 function initTheme() {
+    setWindowBorderUp()
+
     ipcRenderer.on('set-accent-color', (_e, color) => {
         const light = adjustColorBrightness(color, 30)
         const lighter = adjustColorBrightness(color, 60)
--- src/ui.js
+++ src/ui.js
@@ -1,14 +1,3 @@
-const addressBar = document.getElementById('address-bar')
-const fileList = document.getElementById('file-list')
-
-const backButton = document.getElementById('back-button')
-const forwardButton = document.getElementById('forward-button')
-const refreshButton = document.getElementById('refresh-button')
-
-const contextMenu = document.getElementById('context-menu')
-
-const fileItems = () => Array.from(document.querySelectorAll('.file-item'))
-
 function getUI() {
     return {
         contextMenu: document.getElementById('context-menu'),
@@ -17,6 +6,7 @@ function getUI() {
         backButton: document.getElementById('back-button'),
         forwardButton: document.getElementById('forward-button'),
         refreshButton: document.getElementById('refresh-button'),
+        border: document.getElementById('window-border'),
         fileItems: () => Array.from(document.querySelectorAll('.file-item'))
     }
 }
